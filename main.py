edf0a526eee744570152cedc4c866f2c62a27e39
Cole Ballard <cole.ballard97@gmail.com>
2022-07-05 15:46:19 -0500

    finished part 1

--- main.py
+++ main.py
@@ -37,11 +37,24 @@ def main():
 
     police_reports = list(filter(lambda a: a["totalresponsetime"] != "", police_reports))
 
-    print(police_reports[0:2])
+    police_reports = list(filter(lambda a: a["dispatchtime"] != "", police_reports))
 
-    average_response_time = sum(float(d['totalresponsetime']) for d in police_reports) / len(police_reports)
+    police_reports = list(filter(lambda a: a["totaltime"] != "", police_reports))
 
-    print(average_response_time)
+    average_response_time = reduce(lambda aggregate_value, next_row: aggregate_value + float(next_row["totalresponsetime"]), police_reports, 0) / len(police_reports)
+    
+    average_dispatch_time = reduce(lambda aggregate_value, next_row: aggregate_value + float(next_row["dispatchtime"]), police_reports, 0) / len(police_reports)
+
+    average_total_time = reduce(lambda aggregate_value, next_row: aggregate_value + float(next_row["totaltime"]), police_reports, 0) / len(police_reports)
+
+    # average_response_time = sum(float(d['totalresponsetime']) for d in police_reports) / len(police_reports)
+
+    print("Part 1")
+    print("Average Response Time: " + str(average_response_time))
+    print("Average Dispatch Time: " + str(average_dispatch_time))
+    print("Average Total Time: " + str(average_total_time))
+
+
 
 if __name__ == "__main__":
     main()
